62b3b2bdb4623bf424725ba4299e5155049ce687
guineitor <guineitor.traps236@gmail.com>
2020-07-07 19:08:54 -0300

    aded posts

--- go/main.go
+++ go/main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
     "html/template"
-    "net/http"
+	"net/http"
 )
+
+type Post struct {
+    Titulo    string   `json:"titulo"`
+	SubTitulo string   `json:"sub_titulo"`
+	Conteudo  string   `json:"conteudo"`
+	Fotos     string `json:"fotos"`
+	Autor     string   `json:"autor"`
+	Data      string   `json:"data"`
+	Permalink string   `json:"permalink"`
+}
+
+type Posts struct {
+    Posts     []Post
+}
+
+func FindPost()[]Post {
+
+	jsonFile, err := os.Open("../data/posts.json")
+	if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Successfully Opened posts.json")
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	
+	var p []Post
+
+	json.Unmarshal(byteValue, &p)
+
+	return p
+}
+
+func GetPosts() Posts {
+	data := Posts{
+		Posts: FindPost(),
+	}
+	return data
+}
 
 func main() {
 	
@@ -12,14 +56,11 @@ func main() {
 	blog := template.Must(template.ParseFiles("template/blog.html"))
 	_404 := template.Must(template.ParseFiles("template/404.html"))
 
-
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-
-
-	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("index")
 		if r.Method != http.MethodGet {
             index.Execute(w, nil)
             return
@@ -28,6 +69,7 @@ func main() {
 	})
 
 	http.HandleFunc("/manifesto", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("manifesto")
 		if r.Method != http.MethodGet {
             _404.Execute(w, nil)
             return
@@ -36,14 +78,19 @@ func main() {
 	})
 
 	http.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
+		fmt.Println("blog")
+		if r.Method != http.MethodGet {	
             _404.Execute(w, nil)
             return
-        }
-		blog.Execute(w, struct{ Success bool }{true})
+		}
+
+		data := GetPosts()
+
+		blog.Execute(w, data)
 	})
 
 	http.HandleFunc("/not-found", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("not-found")
 		if r.Method != http.MethodGet {
             _404.Execute(w, nil)
             return
@@ -51,6 +98,18 @@ func main() {
 		_404.Execute(w, struct{ Success bool }{true})
 	})
 
-    http.ListenAndServe(":80", nil)
+	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("not-found")
+		if r.Method != http.MethodGet {
+            _404.Execute(w, nil)
+            return
+        }
+		_404.Execute(w, struct{ Success bool }{true})
+	})
+
+	
+
+	http.ListenAndServe(":8082", nil)
+	
 
 }
