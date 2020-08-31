ff0b71910b43f3dd8cb2318167a8a448105497f8
Guilherme  Araujo Sousa <guilhermearaujosousa@Mac-mini-de-Guilherme.local>
2020-08-31 11:19:31 -0300

    added port for nginx deploy

--- go/main.go
+++ go/main.go
@@ -2,39 +2,39 @@ package main
 
 import (
 	"encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "html/template"
+	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 type Post struct {
-    Titulo    string   `json:"titulo"`
-	SubTitulo string   `json:"sub_titulo"`
-	Conteudo  string   `json:"conteudo"`
+	Titulo    string `json:"titulo"`
+	SubTitulo string `json:"sub_titulo"`
+	Conteudo  string `json:"conteudo"`
 	Fotos     string `json:"fotos"`
-	Autor     string   `json:"autor"`
-	Data      string   `json:"data"`
-	Permalink string   `json:"permalink"`
+	Autor     string `json:"autor"`
+	Data      string `json:"data"`
+	Permalink string `json:"permalink"`
 }
 
 type Posts struct {
-    Posts     []Post
+	Posts []Post
 }
 
-func FindPost()[]Post {
+func FindPost() []Post {
 
 	jsonFile, err := os.Open("../data/posts.json")
 	if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println("Successfully Opened posts.json")
-    // defer the closing of our jsonFile so that we can parse it later on
-    defer jsonFile.Close()
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened posts.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	
+
 	var p []Post
 
 	json.Unmarshal(byteValue, &p)
@@ -50,7 +50,7 @@ func GetPosts() Posts {
 }
 
 func main() {
-	
+
 	index := template.Must(template.ParseFiles("template/index.html"))
 	manifesto := template.Must(template.ParseFiles("template/manifesto.html"))
 	blog := template.Must(template.ParseFiles("template/blog.html"))
@@ -62,26 +62,26 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("index")
 		if r.Method != http.MethodGet {
-            index.Execute(w, nil)
-            return
-        }
+			index.Execute(w, nil)
+			return
+		}
 		index.Execute(w, struct{ Success bool }{true})
 	})
 
 	http.HandleFunc("/manifesto", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("manifesto")
 		if r.Method != http.MethodGet {
-            _404.Execute(w, nil)
-            return
-        }
+			_404.Execute(w, nil)
+			return
+		}
 		manifesto.Execute(w, struct{ Success bool }{true})
 	})
 
 	http.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("blog")
-		if r.Method != http.MethodGet {	
-            _404.Execute(w, nil)
-            return
+		if r.Method != http.MethodGet {
+			_404.Execute(w, nil)
+			return
 		}
 
 		data := GetPosts()
@@ -92,24 +92,21 @@ func main() {
 	http.HandleFunc("/not-found", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("not-found")
 		if r.Method != http.MethodGet {
-            _404.Execute(w, nil)
-            return
-        }
+			_404.Execute(w, nil)
+			return
+		}
 		_404.Execute(w, struct{ Success bool }{true})
 	})
 
 	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("not-found")
 		if r.Method != http.MethodGet {
-            _404.Execute(w, nil)
-            return
-        }
+			_404.Execute(w, nil)
+			return
+		}
 		_404.Execute(w, struct{ Success bool }{true})
 	})
 
-	
-
-	http.ListenAndServe(":8082", nil)
-	
+	http.ListenAndServe(":9990", nil)
 
 }
